79f55019d08b5ffdeee14059c92278927c356f0d
Krinsi Kayada <81806503+Kayadakrinsi@users.noreply.github.com>
2022-06-11 19:56:54 +0530

    Add files via upload

---
+++ tower_of_honoi.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ void toh(int n,int source,int destination,int auxiliary){
+     if(n>0){
+         toh(n-1,source,auxiliary,destination);
+            cout<<"Move disk "<<n<<" from "<<source<<" to "<<destination<<endl;
+        toh(n-1,auxiliary,destination,source);
+     }
+ }
+int main()
+{
+     toh(3,1,3,2); 
+     return 0;
+}
